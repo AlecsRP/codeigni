dc50fe0243c85ffeafe9fcd915e06c9032d936c1
repo <evpes2020@gmail.com>
2025-10-23 23:08:39 -0600

    IGNITER

---
+++ app/Views/home_view.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>?= esc($title) ?</title>
+</head>
+<body>
+	<h1>Hola mundo desde CodeIgniter 4 para Progra 2</h1>
+	<p>Vista creada y renderizada con CodeIgniter 4</p>
+</body>
+</html>
